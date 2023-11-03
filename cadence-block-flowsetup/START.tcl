e9fcefe88509f6b492ed2501799f75d7127483c2
Akash Levy <akashlevy@gmail.com>
2023-11-02 17:53:05 -0700

    Go

--- cadence-block-flowsetup/START.tcl
+++ cadence-block-flowsetup/START.tcl
@@ -1,2 +1,1 @@
-puts "Features: $::env(features)"
-write_flow_template -type block -tools {genus innovus quantus tempus voltus lec} -enable_feature { env(features) } -directory outputs
+write_flow_template -type block -tools {genus innovus quantus tempus voltus lec} -enable_feature $::env(features) -directory outputs;
