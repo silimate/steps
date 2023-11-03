1635bd709c26936ce670f057d27931e2d9375d6a
Akash Levy <akashlevy@gmail.com>
2023-11-02 17:29:41 -0700

    Try again

--- cadence-block-flowsetup/START.tcl
+++ cadence-block-flowsetup/START.tcl
@@ -1,1 +1,1 @@
-write_flow_template -type block -tools {genus innovus quantus tempus voltus lec} -enable_feature $::env(features) -directory outputs
+write_flow_template -type block -tools {genus innovus quantus tempus voltus lec} -enable_feature {$::env(features)} -directory outputs
