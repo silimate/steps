851b57225e97cc315201b01928d667aef1406701
Akash Levy <akashlevy@gmail.com>
2023-11-07 00:43:33 -0800

    Again

--- cadence-genus-flowsetup/START.tcl
+++ cadence-genus-flowsetup/START.tcl
@@ -3,5 +3,5 @@ write_flow_template -type block -tools {genus} -enable_feature $::env(features) 
 
 # Substitute process node into genus_config.tcl
 source inputs/mmmc_config.pdk.tcl
-puts Generating the genus_config.tcl file...
+puts "Generating the genus_config.tcl file..."
 exec sed 's/\<\< PLACEHOLDER: PROCESS NODE \>\>/$PROCESS/' outputs/genus_config.template > outputs/genus_config.tcl
