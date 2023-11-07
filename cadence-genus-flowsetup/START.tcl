9c9db209f0e9446a2fbdee139b1f2d776d7d389e
Akash Levy <akashlevy@gmail.com>
2023-11-07 00:24:25 -0800

    Smallfix

--- cadence-genus-flowsetup/START.tcl
+++ cadence-genus-flowsetup/START.tcl
@@ -1,4 +1,6 @@
 # Write out flow template
 write_flow_template -type block -tools {genus} -enable_feature $::env(features) -directory outputs
+
+# Substitute process node into genus_config.tcl
 source inputs/mmmc_config.pdk.tcl
-exec sed 's/<< PLACEHOLDER:\ PROCESS NODE >>/'\"$PROCESS\"'/' outputs/genus_config.template > outputs/genus_config.tcl
+exec sed 's/\<\< PLACEHOLDER: PROCESS NODE \>\>/$PROCESS/' outputs/genus_config.template > outputs/genus_config.tcl
