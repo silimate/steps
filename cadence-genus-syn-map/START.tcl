824a92a4eaf012fd7dd34dc51a9b5a5f19a00d13
Akash Levy <akashlevy@gmail.com>
2023-11-08 19:41:49 -0800

    Add read_dbs

--- cadence-genus-syn-map/START.tcl
+++ cadence-genus-syn-map/START.tcl
@@ -1,3 +1,4 @@
 # Source flows and run technology mapping flow
+read_db inputs/syn_generic.db
 source inputs/run_flow.tcl
 run_flow -flow syn_map
--- cadence-genus-syn-opt/START.tcl
+++ cadence-genus-syn-opt/START.tcl
@@ -1,3 +1,4 @@
 # Source flows and run synthesis optimization flow
+read_db inputs/syn_map.db
 source inputs/run_flow.tcl
 run_flow -flow syn_opt
