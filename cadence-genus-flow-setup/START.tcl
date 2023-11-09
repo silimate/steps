556577bc823918d7f3081931fabf2e7c5c989da0
Akash Levy <akashlevy@gmail.com>
2023-11-08 17:17:12 -0800

    Flow Setup

--- cadence-genus-flow-setup/START.tcl
+++ cadence-genus-flow-setup/START.tcl
@@ -1,7 +1,7 @@
 # Write out flow template
 write_flow_template -type block -tools {genus} -enable_feature $::env(features) -directory outputs
 
-# Substitute process node into genus_config.tcl
+# Insert process node and HDL search path into genus_config.tcl
 source inputs/mmmc_config.pdk.tcl
 puts "Generating the genus_config.tcl file..."
 exec sed "N;s/<< PLACEHOLDER: PROCESS NODE >>/$PROCESS/;s/\\(# HDL attributes.*\\)/\\1\\n  set_db init_hdl_search_path             inputs\\/rtl\\//" outputs/genus_config.template > outputs/genus_config.tcl
