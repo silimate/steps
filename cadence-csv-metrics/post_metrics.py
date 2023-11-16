dc73752c5dbea3250903a5016d8579b60f2ad84a
Akash Levy <akashlevy@gmail.com>
2023-11-16 01:43:11 -0800

    Metrics posting for Cadence

--- cadence-csv-metrics/post_metrics.py
+++ cadence-csv-metrics/post_metrics.py
@@ -10,6 +10,8 @@ def is_float(string):
   except ValueError:
     return False
 
+step = os.readlink("inputs/metrics.csv").split("/")[-3]
+
 # Convert numerical metrics to list of dicts
 metrics = [{
   "design_name": os.environ["DESIGN"],
@@ -20,6 +22,23 @@ metrics = [{
   "metric": metric + (" (" + value.split()[1] + ")" if len(value.split()) > 1 else ""), # Add units to metric name if available
   "value": float(value.split()[0])
 } for row in csv.DictReader(open("inputs/metrics.csv")) for metric, value in row.items() if (len(value.split()) > 0) and is_float(value.split()[0])] # Split to get units if available
+
+# Metrics to be posted
+print(metrics)
+
+# Post metrics to API for storage in database
+print(requests.post("http://localhost/api/metrics/", json=metrics))
+
+# Convert numerical metrics to list of dicts
+metrics = [{
+  "design_name": os.environ["DESIGN"],
+  "flow": os.environ["DESIGN"],
+  "run": os.environ["FLOW_RUNID"],
+  "step": step,
+  "time": time.time() * 1000,
+  "metric": open("inputs/metrics.json"),
+  "value": -98765.4321
+}]
 
 # Metrics to be posted
 print(metrics)
