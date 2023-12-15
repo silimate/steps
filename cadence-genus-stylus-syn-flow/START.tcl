e1e09198b27f40a28610c761a2b346c1419fe2ab
Akash Levy <akashlevy@gmail.com>
2023-12-15 10:55:04 -0800

    Go

--- cadence-genus-stylus-syn-flow/START.tcl
+++ cadence-genus-stylus-syn-flow/START.tcl
@@ -1,8 +1,10 @@
-# Source flows and run block flow
+# Source flows
 source inputs/flow/common_steps.tcl
 source inputs/flow/common_flows.tcl
 source inputs/flow/genus_steps.tcl
 source inputs/config/genus_config.tcl
 source inputs/config/flow_config.tcl
 source inputs/config/design_config.tcl
+
+# Run block flow
 run_flow -flow block
