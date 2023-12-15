03d9c9fd9d244fcc3a146747d41a1a705a033519
Akash Levy <akashlevy@gmail.com>
2023-12-15 10:44:38 -0800

    Swap order and retry

--- cadence-genus-stylus-syn-flow/START.tcl
+++ cadence-genus-stylus-syn-flow/START.tcl
@@ -2,6 +2,6 @@
 source inputs/flow/common_steps.tcl
 source inputs/flow/common_flows.tcl
 source inputs/flow/genus_steps.tcl
-source inputs/config/design_config.tcl
 source inputs/config/genus_config.tcl
+source inputs/config/design_config.tcl
 run_flow -flow block
