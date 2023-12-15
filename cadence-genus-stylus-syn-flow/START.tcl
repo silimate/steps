c64fb8bdccf5c08a26108b131644d2638f690564
Akash Levy <akashlevy@gmail.com>
2023-12-15 10:54:43 -0800

    Try again, no new line before

--- cadence-genus-stylus-syn-flow/START.tcl
+++ cadence-genus-stylus-syn-flow/START.tcl
@@ -5,6 +5,4 @@ source inputs/flow/genus_steps.tcl
 source inputs/config/genus_config.tcl
 source inputs/config/flow_config.tcl
 source inputs/config/design_config.tcl
-puts "RUNNING BLOCK FLOW"
 run_flow -flow block
-puts "DONE RUNNING BLOCK FLOW"
