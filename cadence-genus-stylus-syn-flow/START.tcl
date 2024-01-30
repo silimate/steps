7fe7c8e06aeeb8b1d0ef11e12e5f7257dd6d2a49
Akash Levy <akashlevy@gmail.com>
2024-01-30 04:59:52 -0800

    Source innovus config

--- cadence-genus-stylus-syn-flow/START.tcl
+++ cadence-genus-stylus-syn-flow/START.tcl
@@ -3,6 +3,9 @@ source inputs/flow/common_steps.tcl
 source inputs/flow/common_flows.tcl
 source inputs/flow/genus_steps.tcl
 source inputs/config/genus_config.tcl
+if { [file exists inputs/config/innovus_config.tcl] } {
+  source inputs/config/innovus_config.tcl
+}
 source inputs/config/flow_config.tcl
 source inputs/config/design_config.tcl
 
