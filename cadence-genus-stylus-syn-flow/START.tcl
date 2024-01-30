8d4cc4688a14729ca17812d284b508ea927b8f12
Akash Levy <akashlevy@gmail.com>
2024-01-30 06:43:55 -0800

    Try again

--- cadence-genus-stylus-syn-flow/START.tcl
+++ cadence-genus-stylus-syn-flow/START.tcl
@@ -1,12 +1,5 @@
-# Source flows
-source inputs/flow/common_steps.tcl
-source inputs/flow/common_flows.tcl
-source inputs/flow/genus_steps.tcl
-
-# Source configs
-source inputs/config/genus_config.tcl
-source inputs/config/flow_config.tcl
-source inputs/config/design_config.tcl
+# Read flow
+read_flow inputs/config/flow.yaml
 
 # Run synthesis flow
-run_flow -flow synthesis -yaml inputs/config/setup.yaml
+run_flow -flow synthesis
