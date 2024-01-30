025be2edfb4185e5150d710cf205e2d586f8ee8b
Akash Levy <akashlevy@gmail.com>
2024-01-30 05:18:43 -0800

    Just run synthesis not implementation

--- cadence-genus-stylus-syn-flow/START.tcl
+++ cadence-genus-stylus-syn-flow/START.tcl
@@ -2,17 +2,11 @@
 source inputs/flow/common_steps.tcl
 source inputs/flow/common_flows.tcl
 source inputs/flow/genus_steps.tcl
-if { [file exists inputs/flow/innovus_steps.tcl] } {
-  source inputs/flow/innovus_steps.tcl
-}
 
 # Source configs
 source inputs/config/genus_config.tcl
-if { [file exists inputs/config/innovus_config.tcl] } {
-  source inputs/config/innovus_config.tcl
-}
 source inputs/config/flow_config.tcl
 source inputs/config/design_config.tcl
 
-# Run block flow
-run_flow -flow block
+# Run synthesis flow
+run_flow -flow synthesis
