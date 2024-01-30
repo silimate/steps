2d8e9f6ddbf07ec42d1c56d953df9869084f61a1
Akash Levy <akashlevy@gmail.com>
2024-01-30 02:42:57 -0800

    Cadence Stylus flow setup fixes

--- cadence-genus-stylus-flow-setup/START.tcl
+++
@@ -1,2 +0,0 @@
-# Write out flow templates
-write_flow_template -type stylus -tools {genus} -enable_feature $::env(features) -directory outputs
---
+++ cadence-stylus-flow-setup/START.tcl
@@ -0,0 +1,2 @@
+# Write out flow templates
+write_flow_template -type stylus -tools $::env(tools) -enable_feature $::env(features) -directory outputs
