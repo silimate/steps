17337524211e78b9a209c95c6c5bf43ba667b312
Akash Levy <akashlevy@gmail.com>
2024-02-01 00:09:22 -0800

    optional_feature no s

--- cadence-stylus-flow-setup/START.tcl
+++ cadence-stylus-flow-setup/START.tcl
@@ -1,2 +1,2 @@
 # Write out flow templates
-write_flow_template -type stylus -tools $::env(tools) -enable_feature $::env(enabled_features) -optional_features $::env(optional_features) -directory outputs
+write_flow_template -type stylus -tools $::env(tools) -enable_feature $::env(enabled_features) -optional_feature $::env(optional_features) -directory outputs
