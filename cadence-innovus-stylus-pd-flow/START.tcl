a4d2154a1adb1735b618ba6209c4f3621013fc42
Akash Levy <akashlevy@gmail.com>
2024-01-31 23:30:42 -0800

    Don't load Genus DB

--- cadence-innovus-stylus-pd-flow/START.tcl
+++ cadence-innovus-stylus-pd-flow/START.tcl
@@ -1,8 +1,8 @@
 # Read flow
 read_flow inputs/flow.yaml
 
-# Read Genus DB
-read_db inputs/genus.db
+# # Read Genus DB
+# read_db inputs/genus.db
 
 # Run implementation flow
 run_flow -flow implementation
