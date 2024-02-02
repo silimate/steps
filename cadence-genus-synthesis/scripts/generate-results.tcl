d569edb8a653007f4432b7e1061ee86d71c50324
Akash Levy <akashlevy@gmail.com>
2024-02-01 23:41:34 -0800

    Improve

--- cadence-genus-synthesis/scripts/generate-results.tcl
+++ cadence-genus-synthesis/scripts/generate-results.tcl
@@ -10,10 +10,8 @@ if { $uniquify_with_design_name == True } {
   update_names -subdesign -force -prefix ${design_name}_
 }
 
-write_snapshot -directory results_syn -tag final
-write_design -innovus -basename results_syn/syn_out
-write_sdf > results_syn/syn_out.sdf
-write_spef > results_syn/syn_out.spef
-
-write_name_mapping
-
+write_snapshot -directory outputs -tag final
+write_design -innovus -basename outputs/design
+write_sdf > outputs/design.sdf
+write_spef > outputs/design.spef
+write_name_mapping -hdl -hierarchical -initial -map_pins all -print_deleted -output output design.namemap
