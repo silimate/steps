b9d48175fd4f0ac92ca14d7636166e68ec2b12b4
Akash Levy <akashlevy@gmail.com>
2024-02-09 18:03:24 +0800

    Read DB from genus.cdb

--- cadence-innovus-stylus-pd-flow/START.tcl
+++ cadence-innovus-stylus-pd-flow/START.tcl
@@ -2,9 +2,7 @@
 read_flow inputs/flow.yaml
 
 # Read design information from Genus
-source inputs/dbs/genus.cdb/cmn/*.mmmc.tcl
-set_db init_netlist_files inputs/dbs/genus.cdb/cmn/*.v.gz
-read_db inputs/dbs/genus.cdb/cmn/
+read_db inputs/dbs/genus.cdb/
 
 # Run implementation flow
 run_flow -flow implementation
