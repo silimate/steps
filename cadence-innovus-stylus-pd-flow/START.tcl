e8e02a5c819ee395310c69c3187c4ffccc1215fa
Akash Levy <akashlevy@gmail.com>
2024-02-09 16:08:27 +0800

    Try with /cmn

--- cadence-innovus-stylus-pd-flow/START.tcl
+++ cadence-innovus-stylus-pd-flow/START.tcl
@@ -2,7 +2,7 @@
 read_flow inputs/flow.yaml
 
 # Read design information from Genus
-read_db inputs/dbs/genus.cdb
+read_db inputs/dbs/genus.cdb/cmn/
 
 # Run implementation flow
 run_flow -flow implementation
