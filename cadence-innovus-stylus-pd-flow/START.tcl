e7751207da7a429379c8678c96e63c9fbe62a905
Akash Levy <akashlevy@gmail.com>
2024-02-09 15:51:22 +0800

    Commit

--- cadence-innovus-stylus-pd-flow/START.tcl
+++ cadence-innovus-stylus-pd-flow/START.tcl
@@ -2,7 +2,7 @@
 read_flow inputs/flow.yaml
 
 # Read design information from Genus
-read_design inputs/dbs/genus.cdb [get_db current_design .name]
+read_db inputs/dbs/genus.cdb
 
 # Run implementation flow
 run_flow -flow implementation
